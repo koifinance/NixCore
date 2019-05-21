e1764c9acafa76d2a371cbe9657d69781bc13dfd
mattt21 <matt@nixplatform.io>
2019-05-20 20:30:43 -0500

    fix verify check

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -4182,7 +4182,7 @@ UniValue ghostamount(const JSONRPCRequest& request)
     if (request.fHelp || request.params.size() > 2)
         throw runtime_error("ghostamount <amount>(whole numbers only) <commitment key pack>\n" + HelpRequiringPassphrase(pwalletMain));
 
-    if(fDisableZerocoinTransactions)
+    if(false)
         throw JSONRPCError(RPC_WALLET_ERROR, "ghosted tranasactions are not currently being accepted");
 
     int64_t nAmount = request.params[0].get_int64();
